9cb877424f00a51c13024a44b5d2175f149c1584
yuantang126 <yuantang126@126.com>
2018-04-16 18:17:59 +0800

    ....

--- app/src/main/java/com/coder/relax/MyApp.java
+++ app/src/main/java/com/coder/relax/MyApp.java
@@ -2,6 +2,7 @@ package com.coder.relax;
 
 import android.app.Application;
 
+import com.google.android.gms.ads.MobileAds;
 import com.tencent.bugly.crashreport.CrashReport;
 
 /**
@@ -17,6 +18,6 @@ public class MyApp extends Application {
         strategy.setAppChannel("google play");
         CrashReport.initCrashReport(getApplicationContext(), "2174eb55bf", false,strategy);
         CrashReport.setIsDevelopmentDevice(this, false);
-//        MobileAds.initialize(this,"ca-app-pub-4727610544131155~6488840515");
+        MobileAds.initialize(this,"ca-app-pub-4727610544131155~6488840515");
     }
 }
